077a7a4957e81109f6333a8e60ee9820f7684b02
Pablo Álvarez Córdoba <pabloalvarezcordoba@protonmail.ch>
2018-03-21 16:48:08 +0100

    .

--- src/main/java/es/achosoftware/ifreedays/config/Security.java
+++ src/main/java/es/achosoftware/ifreedays/config/Security.java
@@ -44,13 +44,13 @@ public class Security extends WebSecurityConfigurerAdapter {
 				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
 				.exceptionHandling().accessDeniedPage("/access-denied");
 
-//		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
-//				.antMatchers("/registration").permitAll().antMatchers("/console/**").permitAll()
-//				.antMatchers("/skills/**").authenticated().antMatchers("/user/**").hasAuthority("USER").anyRequest()
-//				.authenticated().and().csrf().disable().formLogin().loginPage("/login").failureUrl("/login?error=true")
-//				.defaultSuccessUrl("/checkType").usernameParameter("email").passwordParameter("password").and().logout()
-//				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
-//				.exceptionHandling().accessDeniedPage("/access-denied");
+		http.authorizeRequests().antMatchers("/").permitAll().antMatchers("/login").permitAll()
+				.antMatchers("/registration").permitAll().antMatchers("/console/**").permitAll()
+				.antMatchers("/skills/**").authenticated().antMatchers("/user/**").hasAuthority("USER").anyRequest()
+				.authenticated().and().csrf().disable().formLogin().loginPage("/login").failureUrl("/login?error=true")
+				.defaultSuccessUrl("/checkType").usernameParameter("email").passwordParameter("password").and().logout()
+				.logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/").and()
+				.exceptionHandling().accessDeniedPage("/access-denied");
 
 //		http.authorizeRequests().antMatchers("/skills/**").hasAuthority("ADMIN").anyRequest().authenticated().and()
 //				.csrf().disable().formLogin().loginPage("/login").failureUrl("/login?error=true")
