97e3a668a76dd83da90b41cd971c1f141ca0031e
Miralles Pérez <antonio.miralles.perez.1@gmail.com>
2018-03-27 09:27:37 +0200

    tableThing

--- src/main/java/es/achosoftware/ifreedays/repository/ProjectsRepository.java
+++ src/main/java/es/achosoftware/ifreedays/repository/ProjectsRepository.java
@@ -1,5 +1,17 @@
 package es.achosoftware.ifreedays.repository;
 
-public class ProjectsRepository {
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import es.achosoftware.ifreedays.model.Project;
+
+@Repository("projectsRepository")
+public interface ProjectsRepository extends JpaRepository<Project, Integer>{
+	
+	List<Project> findProjectsByUserId(int userId);
+	
+	
 
 }
